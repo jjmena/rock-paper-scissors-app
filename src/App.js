9371336732e8c3e320c8dcbef65d09b6e6efdf7c
Jacinto Mena <jacinto.mena@thomascookonline.com>
2019-09-27 13:15:02 +0200

    First version front game. Created REST calls and update table.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    userId: null,
+    rounds: []
+  }
+
+  componentDidMount() {    
+    fetch('http://localhost:8080/user', {method: 'POST'})
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ userId: data.userId })
+    })
+    .catch(console.log)
+  }
+
+  playRound = () => {
+    fetch('http://localhost:8080/round/user/' + this.state.userId, {method: 'POST'})    
+    .then(res => res.json())
+    .then((data) => {
+      this.state.rounds.push(data)
+      this.setState({ rounds: this.state.rounds})      
+    })
+    .catch(console.log)
+  }
+
+  resetRounds = () => {
+    fetch('http://localhost:8080/round/user/' + this.state.userId, {method: 'DELETE'})        
+    .then((data) => {
+      this.setState({ rounds: []})      
+    })
+    .catch(console.log)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h3>Paper, Rock, Scissor Game</h3>
+          <h3>Paper, Rock, Scissor Game. Welcome {this.state.userId}</h3>          
         </header>
         <section>
-          <div class="App-result-table">
+          <div className="App-result-table">
             <table>
               <thead>
                 <tr>
@@ -19,17 +51,21 @@ class App extends Component {
                   <th>Game Result</th>
                 </tr>
               </thead>
-              <tbody>
-                <tr>
-                  <td>ROCK</td>
-                  <td>ROCK</td>
-                  <td>DRAW</td>
-                </tr>
+              <tbody>                
+              {this.state.rounds.map(function (value, index) {             
+                return(           
+                      <tr>
+                          <td>{value.firstPlayerOption}</td>
+                          <td>{value.secondPlayerOption}</td>
+                          <td>{value.result}</td>
+                      </tr>
+                      )                          
+                    })}                
               </tbody>
-            </table>
+            </table>            
           </div>
-          <button type="button">Play Round</button>
-          <button type="button">Reset Game</button>
+          <button type="button" onClick={this.playRound}>Play Round</button>
+          <button type="button" onClick={this.resetRounds}>Reset Game</button>
         </section>        
       </div>
     );
