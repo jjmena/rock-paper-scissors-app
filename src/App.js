513ae4618f24132fd96322a775b89f07ea48bfbd
Jacinto Mena <jacinto.mena@thomascookonline.com>
2019-09-30 11:10:51 +0200

    CSS changes. Create ENV variable to configure API endpoint HOST.

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
     rounds: [],
     stats: {draw: 0, winner1: 0, winner2: 0, total: 0}
   }
-
-  componentDidMount() {    
-    fetch('http://localhost:8080/user', {method: 'POST'})
+  
+  componentDidMount() {
+    fetch(process.env.REACT_APP_API_HOST + 'user', {method: 'POST'})
     .then(res => res.json())
     .then((data) => {
       this.setState({ userId: data.userId })
@@ -21,9 +21,10 @@ class App extends Component {
   }
 
   playRound = () => {
-    fetch('http://localhost:8080/round/user/' + this.state.userId, {method: 'POST'})    
+    fetch(process.env.REACT_APP_API_HOST + 'round/user/' + this.state.userId, {method: 'POST'})    
     .then(res => res.json())
     .then((data) => {
+      data.translatedResult = this.translateResult(data.result)
       this.state.rounds.push(data)
       this.setState({ rounds: this.state.rounds})
       this.updateStats()      
@@ -32,7 +33,7 @@ class App extends Component {
   }
 
   resetRounds = () => {
-    fetch('http://localhost:8080/round/user/' + this.state.userId, {method: 'DELETE'})        
+    fetch(process.env.REACT_APP_API_HOST + 'round/user/' + this.state.userId, {method: 'DELETE'})        
     .then((data) => {
       this.setState({ rounds: []})
       this.updateStats()      
@@ -41,7 +42,7 @@ class App extends Component {
   }
 
   updateStats = () => {
-    fetch('http://localhost:8080/stats', {method: 'GET'})    
+    fetch(process.env.REACT_APP_API_HOST + 'stats', {method: 'GET'})    
     .then(res => res.json())
     .then((data) => {
       this.setState({ stats: {draw: data.DRAW, winner1: data.WINNER_PLAYER1, winner2: data.WINNER_PLAYER2, total: data.DRAW + data.WINNER_PLAYER1 + data.WINNER_PLAYER2}})      
@@ -49,15 +50,22 @@ class App extends Component {
     .catch(console.log)
   }
 
+  translateResult = (r) => {    
+    if (r === 'WINNER_PLAYER1') return "Player 1 wins"
+    if (r === 'WINNER_PLAYER2') return "Player 2 wins"
+    return "Draw game"
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h3>Paper, Rock, Scissor Game. Welcome {this.state.userId}</h3>          
+        <header className="header">
+          <h2>Welcome to Paper, Rock, Scissor Game</h2>          
         </header>
         <section>
-          <div>
-            <table align="center">
+          <h3>Game Statistics</h3>
+          <div className="result-table">
+            <table>
               <thead>
                 <tr>
                   <th>Total Rounds</th>
@@ -79,13 +87,14 @@ class App extends Component {
         </section>
         <br/>
         <section>
-          <div className="App-result-table">
-            <table align="center">
+          <h3>Games Played</h3>
+          <div className="result-table">
+            <table>
               <thead>
                 <tr>
-                  <th>Player 1 Option</th>
-                  <th>Player 2 Option</th>
-                  <th>Game Result</th>
+                  <th>Player 1</th>
+                  <th>Player 2</th>
+                  <th>Result</th>
                 </tr>
               </thead>
               <tbody>                
@@ -94,7 +103,7 @@ class App extends Component {
                       <tr>
                           <td>{value.firstPlayerOption}</td>
                           <td>{value.secondPlayerOption}</td>
-                          <td>{value.result}</td>
+                          <td>{value.translatedResult}</td>
                       </tr>
                       )                          
                     })}                
@@ -102,9 +111,12 @@ class App extends Component {
             </table>            
           </div>
           <br/>
-          <button type="button" onClick={this.playRound}>Play Round</button>
-          <button type="button" onClick={this.resetRounds}>Reset Game</button>
-        </section>        
+          <button type="button" onClick={this.playRound} className="button">Play Round</button>
+          <button type="button" onClick={this.resetRounds} className="button">Reset Game</button>
+        </section>    
+        <footer className="footer">
+          <p>Session identifier: {this.state.userId}</p>          
+        </footer>    
       </div>
     );
   }
